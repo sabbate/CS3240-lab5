bb58599d82488ac6b712c9c3efc12504474a4a30
Sam Abbate <sja4cm@virginia.edu>
2015-02-23 16:24:08 -0500

    course1.db has been filled in with data from the csv file

--- course_reader.py
+++ course_reader.py
@@ -1,10 +1,15 @@
 import csv
+import sqlite3
 __author__ = 'Sam'
 
 def load_course_database(db_name, csv_filename):
-    with open(csv_filename, 'rU'):
-        reader = csv.reader(csv_filename);
+    with open(csv_filename, 'rU') as file:
+        reader = csv.reader(file);
         for row in reader:
-            print(row);
+            conn = sqlite3.connect(db_name)
+            with conn:
+                cur = conn.cursor();
+                sql_cmd = "insert into coursedata values(?, ?, ?, ?, ?, ?, ?)";
+                cur.execute(sql_cmd, (row[0], row[1], row[2], row[3], row[4], row[5], row[6]));
 
 load_course_database('course1.db', 'seas-courses-5years.csv');
